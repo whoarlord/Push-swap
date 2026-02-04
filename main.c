378d8bb0cc8c828ef8f697bb71a66fd72c5fc6c2
Sara <sarahierrovazquez@gmail.com>
2026-02-04 13:35:10 +0100

    function validate numbers added to main

--- main.c
+++ main.c
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iarrien- <iarrien-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: shierro <shierro@student.42urduliz.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 13:04:02 by iarrien-          #+#    #+#             */
-/*   Updated: 2026/02/04 13:09:14 by iarrien-         ###   ########.fr       */
+/*   Updated: 2026/02/04 13:34:39 by shierro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf/ft_printf.h"
+
+int	ft_validate_nums(char **str_array)
+{
+	int		size;
+	int		i;
+	char	*str;
+
+	size = 0;
+	if (!str_array)
+		return (0);
+	while (*str_array)
+	{
+		i = 0;
+		str = *str_array;
+		while (str[i])
+		{
+			if (!ft_isdigit(str[i]) && !(i == 0 && str[i] == '-'))
+			{
+				write(2, "Error\n", 6);
+				return (0);
+			}
+			i++;
+		}
+		size++;
+		str_array++;
+	}
+	return (size);
+}
 
 int	main(int argc, char *argv[])
 {
@@ -25,5 +53,6 @@ int	main(int argc, char *argv[])
 			write(1, "\n", 1);
 			i++;
 		}
+		ft_printf("Total numbers to sort: %d\n", ft_validate_nums(&argv[1]));
 	}
 }
