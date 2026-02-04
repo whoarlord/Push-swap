1befa510e882330287fba9857ba1bbf2f6a27142
iker <arrieniker4@gmail.com>
2026-02-04 14:26:29 +0100

    header

---
+++ push_swap.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iarrien- <iarrien-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/04 13:26:15 by iarrien-          #+#    #+#             */
+/*   Updated: 2026/02/04 14:19:17 by iarrien-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "printf/ft_printf.h"
+typedef struct s_manager
+{
+    int algorithm;
+    int bench;
+    int error;
+    char **numbers;
+}   t_manager;
+#endif
