7696ef0404b6dd37b5e63581a8093d03664d9973
iker <arrieniker4@gmail.com>
2026-02-05 13:01:41 +0100

    push funcionando

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shierro <shierro@student.42urduliz.com>    +#+  +:+       +#+        */
+/*   By: iarrien- <iarrien-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 13:04:02 by iarrien-          #+#    #+#             */
-/*   Updated: 2026/02/05 11:29:19 by shierro          ###   ########.fr       */
+/*   Updated: 2026/02/05 12:57:16 by iarrien-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,10 @@ int	main(int argc, char *argv[])
 		return (write(2, "Error\n", 6), ft_free_all(manager, a, b), 1);
 	ft_printf("nº de args: %d\n flags:%d\n", size, manager->algorithm);
 	ft_print_stack(a);
+	ft_push(b, a);
+	ft_push(b, a);
 	ft_print_stack(b);
+	ft_print_stack(a);
 	ft_printf("Error de doble num? %d\n", ft_check_doubles(a->nums, a->size));
 	ft_free_all(manager, a, b);
 }
